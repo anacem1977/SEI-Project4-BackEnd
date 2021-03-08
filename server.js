5b764067731dd3657bf03e62492ca4e8f025516c
anace-m <maldonadoanaceciliac@johndeere.com>
2021-03-08 12:34:28 -0600

    updated server.js with models and routes

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const routes = require ("./routes");
 
+const origin= require("./models/origin.js");
+const substyle = require("./models/substyle.js");
+const brand = require("./models/brand.js");
+const brewery = require("./models/brewery.js");
+const user = require("./models/user.js");
+
+const corsOptions = {
+  origin: ['http://localhost:3000'], //port for React app
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true, //allows session cookies to be sent back and forth
+  optionsSuccessStatus: 200 //legacy browsers
+}
+
+app.use(cors(corsOptions))
 
 app.use((req, res, next) => {
     console.log("I run for all routes");
     next();
   });
 
-app.listen(process.env.PORT||3000, () => {
+app.use("/origin", routes.origin);
+app.use("/substyle", routes.substyle);
+app.use("/brand", routes.brand);
+app.use("/brewery", routes.brewery);
+app.use("/user", routes.user);
+
+app.listen(process.env.PORT||3005, () => {
     console.log("My beer enciclopedia app is set up and running");
 });
