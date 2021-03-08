0a6a4b0e71d2f7dc2be7543f07287d8422c679a2
anace-m <maldonadoanaceciliac@johndeere.com>
2021-03-08 10:18:04 -0600

    created user model and updated migration files

--- migrations/20210308160540-create-user.js
+++ migrations/20210308160540-create-user.js
@@ -1,27 +1,29 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Brands', {
+    await queryInterface.createTable('Users', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      substyleId: {
-        type: Sequelize.INTEGER
-      },
-      brand: {
+      username: {
+        unique: true,
+        allowNull: false,
         type: Sequelize.STRING
       },
-      breweryId: {
-        type: Sequelize.INTEGER
-      },
-      abv: {
+      password: {
+        allowNull: false,
         type: Sequelize.STRING
       },
-      likes: {
-        type: Sequelize.INTEGER
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
       },
       createdAt: {
         allowNull: false,
@@ -36,6 +38,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Brands');
+    await queryInterface.dropTable('Users');
   }
 };
